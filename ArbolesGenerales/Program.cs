a7b7e24d73e90a93b2b46aba78c93862b23d31c6
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-06-21 14:10:53 -0700

    Se agregaron los nodos en Program

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ArbolesGenerales;
+ArbolGeneral arbol = new ArbolGeneral("A");
+
+Nodo nodoB = arbol.InsertarHijo(arbol.Raiz, "B");
+Nodo nodoC = arbol.InsertarHijo(arbol.Raiz, "C");
+Nodo nodoD = arbol.InsertarHijo(nodoB, "D");
+Nodo nodoE = arbol.InsertarHijo(nodoD, "E");
+Nodo nodoF = arbol.InsertarHijo(nodoD, "F");
+Nodo nodoG = arbol.InsertarHijo(nodoB, "G");
+
+
+
+string arbolCompleto = arbol.ObtenerArbol();
+Console.WriteLine(arbolCompleto);
